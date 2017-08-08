a90cebad5a2199a4636d6b9afeab2081e53bcdf1
zhanwenchen <phil.zhanwen.chen@gmail.com>
2017-08-08 02:59:02 +0000

    added naive filter

--- VenmeWPF/MainWindow.xaml.cs
+++ VenmeWPF/MainWindow.xaml.cs
@@ -22,6 +22,8 @@ namespace VenmeWPF
     public partial class MainWindow : Window
     {
         private VenmeContext _venmeContext = new VenmeContext();
+
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -30,10 +32,38 @@ namespace VenmeWPF
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
 
-            System.Windows.Data.CollectionViewSource venmeContextViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("venmeContextViewSource")));
+            CollectionViewSource venmeContextViewSource = (CollectionViewSource)FindResource("venmeContextViewSource");
             // Load data by setting the CollectionViewSource.Source property:
             _venmeContext.Transactions.Load();
+            venmeContextViewSource.Filter += yourFilter;
             venmeContextViewSource.Source = _venmeContext.Transactions.Local;
+        }
+
+        private void yourFilter(object sender, FilterEventArgs e)
+        {
+            var obj = e.Item as Transaction;
+            if (obj != null)
+                e.Accepted = obj.FromUserId.Equals(1);
+        }
+
+        private void DatePicker_SelectedDateChanged(object sender,
+            SelectionChangedEventArgs e)
+        {
+            // ... Get DatePicker reference.
+            var picker = sender as DatePicker;
+
+            // ... Get nullable DateTime from SelectedDate.
+            DateTime? date = picker.SelectedDate;
+            if (date == null)
+            {
+                // ... A null object.
+                this.Title = "No date";
+            }
+            else
+            {
+                // ... No need to display the time.
+                this.Title = date.Value.ToShortDateString();
+            }
         }
     }
 }
